2c8e83b3c2b2b501008bccbdc2c97fe71e925b76
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-10-28 11:55:00 +0100

    Zajecia4

---
+++ ActivityExample/app/src/main/java/com/android7/myapplication/Activity1.java
@@ -0,0 +1,16 @@
+package com.android7.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linearlayout);
+
+        
+    }
+}
