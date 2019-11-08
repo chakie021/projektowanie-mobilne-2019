3effeaed040e20116453f9b973b9f9fb25095436
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-11-08 17:28:05 +0100

    cd..

--- ActivityExample/app/src/main/java/com/android7/myapplication/Activity1.java
+++ ActivityExample/app/src/main/java/com/android7/myapplication/Activity1.java
@@ -3,25 +3,32 @@ package com.android7.myapplication;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity1 extends AppCompatActivity {
     LinearLayout linearLayout;
+    TextView clickerText;
+
+    int mPoints = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.linearlayout);
 
+        clickerText = findViewById(R.id.clickerText);
+
         linearLayout = findViewById(R.id.linear_layout);
         linearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(Activity1.this,"tekst",Toast.LENGTH_SHORT).show();
+                mPoints++;
+                clickerText.setText(""+mPoints);
             }
         });
+
 
         
     }
