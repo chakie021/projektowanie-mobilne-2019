3deec2cb4b3c802002be31b3e4b4aeb60e84632c
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-11-08 17:15:27 +0100

    cd..

--- ActivityExample/app/src/main/java/com/android7/myapplication/Activity1.java
+++ ActivityExample/app/src/main/java/com/android7/myapplication/Activity1.java
@@ -1,15 +1,27 @@
 package com.android7.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity1 extends AppCompatActivity {
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.linearlayout);
+
+        linearLayout = findViewById(R.id.linear_layout);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Activity1.this,"tekst",Toast.LENGTH_SHORT).show();
+            }
+        });
 
         
     }
