112f8193ae18437c34445cc2eacc243c8de5ca37
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-11-14 21:13:22 +0100

    Zajecia6

---
+++ Zajecia6/ActivityCommunicationDialogs/app/src/main/java/com/android7/activitycommunicationdialogs/ChildActivity.java
@@ -0,0 +1,33 @@
+package com.android7.activitycommunicationdialogs;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChildActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+        androidx.appcompat.widget.Toolbar toolbar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
