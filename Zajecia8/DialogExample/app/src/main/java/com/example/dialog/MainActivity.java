a042c33904f9d134b588352bced6f251a713a333
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-12-06 17:11:31 +0100

    cd..

--- Zajecia8/DialogExample/app/src/main/java/com/example/dialog/MainActivity.java
+++ Zajecia8/DialogExample/app/src/main/java/com/example/dialog/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.dialog;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+
+    View.OnClickListener button0Click = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            openDialog0();
+        }
+    };
+
+    View.OnClickListener button1Click = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            openDialog1();
+        }
+    };
+
+    View.OnClickListener button2Click = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            openDialog2();
+        }
+    };
+
+    View.OnClickListener button3Click = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            openDialog3();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+
+        button0.setOnClickListener(button0Click);
+        button1.setOnClickListener(button1Click);
+        button2.setOnClickListener(button2Click);
+        button3.setOnClickListener(button3Click);
+
+    }
+
+    private void openDialog0(){
+
+    }
+
+    private void openDialog1(){
+
+    }
+
+    private void openDialog2(){
+
+    }
+
+    private void openDialog3(){
+
     }
 }
