3803d4b58da7c23f0dde6935935372917714ad9b
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-03 17:45:19 +0100

    cd..

--- Zajecia8/DialogExample/app/src/main/java/com/example/dialog/MyDialog.java
+++ Zajecia8/DialogExample/app/src/main/java/com/example/dialog/MyDialog.java
@@ -1,6 +1,23 @@
 package com.example.dialog;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 public class MyDialog extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Tytył okna");
+        builder.setMessage("Przykładowy tekst okna dialogowego");
+
+        AlertDialog dialog = builder.create();
+
+        return dialog;
+    }
 }
