e62f8b6ded99768bc22fd1a3a6a4e03dc50bd05c
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-03 17:10:28 +0100

    cd..

--- Zajecia8/DialogExample/app/src/main/java/com/example/dialog/MainActivity.java
+++ Zajecia8/DialogExample/app/src/main/java/com/example/dialog/MainActivity.java
@@ -14,34 +14,6 @@ public class MainActivity extends AppCompatActivity {
     Button button2;
     Button button3;
 
-    View.OnClickListener button0Click = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            openDialog0();
-        }
-    };
-
-    View.OnClickListener button1Click = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            openDialog1();
-        }
-    };
-
-    View.OnClickListener button2Click = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            openDialog2();
-        }
-    };
-
-    View.OnClickListener button3Click = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            openDialog3();
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,10 +24,19 @@ public class MainActivity extends AppCompatActivity {
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
 
-        button0.setOnClickListener(button0Click);
-        button1.setOnClickListener(button1Click);
-        button2.setOnClickListener(button2Click);
-        button3.setOnClickListener(button3Click);
+
+        button0.setOnClickListener((View view)->{
+            openDialog0();
+        });
+        button1.setOnClickListener((View view)->{
+            openDialog1();
+        });
+        button2.setOnClickListener((View view)->{
+            openDialog2();
+        });
+        button3.setOnClickListener((View view)->{
+            openDialog3();
+        });
 
     }
 
@@ -93,7 +74,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void openDialog2(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Tytył okna");
+        builder.setMessage("Przykładowy tekst okna dialogowego");
 
+        AlertDialog dialog = builder.create();
+
+        dialog.show();
     }
 
     private void openDialog3(){
