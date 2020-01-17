a02a1544a0b5be9b6cfec638f6aec1c71d8e5593
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-17 17:44:56 +0100

    cd..

--- Zajecia12/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/MainActivity.java
+++ Zajecia12/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/MainActivity.java
@@ -3,6 +3,7 @@ package com.e.constraintlayoutexample;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(MainActivity.this,"Button..",Toast.LENGTH_SHORT).show();
             }
         });
 
