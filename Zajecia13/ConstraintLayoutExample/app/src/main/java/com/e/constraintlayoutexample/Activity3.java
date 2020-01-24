92cf156358cd49427ae87a0d54ecee3b9c761cc2
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-24 17:09:59 +0100

    cd..

--- Zajecia13/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/Activity3.java
+++ Zajecia13/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/Activity3.java
@@ -10,6 +10,7 @@ public class Activity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity3_activity);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, new Fragment1())
