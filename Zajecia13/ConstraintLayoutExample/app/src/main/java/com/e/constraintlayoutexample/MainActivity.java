e19a85ac4abc87dddb996f3d650ec022a2f4e6ad
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-24 16:53:13 +0100

    cd..

--- Zajecia13/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/MainActivity.java
+++ Zajecia13/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.e.constraintlayoutexample;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button0 = findViewById(R.id.button1);
+        final Button button1 = findViewById(R.id.button2);
+
+
     }
 }
