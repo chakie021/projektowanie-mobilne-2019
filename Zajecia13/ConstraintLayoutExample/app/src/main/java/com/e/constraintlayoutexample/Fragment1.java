1722ee7bd1d63e28839fa24c64f521b4714a5bfd
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-24 17:24:37 +0100

    cd..

--- Zajecia13/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/Fragment1.java
+++ Zajecia13/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/Fragment1.java
@@ -1,6 +1,24 @@
 package com.e.constraintlayoutexample;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import androidx.fragment.app.Fragment;
 
 public class Fragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        TextView view = new TextView(getContext());
+        view.setText("Jakis napis");
+
+        return view;
+    }
 }
