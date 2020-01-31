af2479bdd298c812d278ccf44e755bf13e997365
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-31 17:09:12 +0100

    cd..

--- Zajecia13/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/Fragment2.java
+++ Zajecia13/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/Fragment2.java
@@ -5,17 +5,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageClickListener;
 import com.synnapps.carouselview.ImageListener;
 
 public class Fragment2 extends Fragment {
 
     CarouselView carouselView;
+    TextView textView;
 
     @Nullable
     @Override
@@ -24,12 +27,22 @@ public class Fragment2 extends Fragment {
         View view = inflater.inflate(R.layout.fragment2,container,false);
 
         carouselView = view.findViewById(R.id.carouselView);
+        textView = view.findViewById(R.id.text1);
+
         carouselView.setPageCount(2);
 
         carouselView.setImageListener(new ImageListener() {
             @Override
             public void setImageForPosition(int position, ImageView imageView) {
                 imageView.setImageResource(R.drawable.ic_android);
+
+            }
+        });
+
+        carouselView.setImageClickListener(new ImageClickListener() {
+            @Override
+            public void onClick(int position) {
+                textView.setText("Numer obrazka: "+position);
             }
         });
 
