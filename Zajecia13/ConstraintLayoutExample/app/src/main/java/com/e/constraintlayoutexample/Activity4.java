1d96fe29cf6a18724630302265851c918306e565
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-31 16:33:23 +0100

    cd..

--- Zajecia13/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/Activity4.java
+++ Zajecia13/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/Activity4.java
@@ -1,15 +1,23 @@
 package com.e.constraintlayoutexample;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity4 extends AppCompatActivity {
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity4_activity);
+
+        frameLayout = findViewById(R.id.container);
+        frameLayout.setBackgroundColor(Color.CYAN);
+
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, new Fragment2())
--- Zajecia13/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/Fragment2.java
+++ Zajecia13/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/Fragment2.java
@@ -3,4 +3,5 @@ package com.e.constraintlayoutexample;
 import androidx.fragment.app.Fragment;
 
 public class Fragment2 extends Fragment {
+
 }
