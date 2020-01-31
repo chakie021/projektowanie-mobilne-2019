e14857bd7ed5a793e5e1f771245b2a382262c693
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-31 17:03:47 +0100

    cd..

--- Zajecia13/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/Fragment2.java
+++ Zajecia13/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/Fragment2.java
@@ -4,17 +4,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+
 public class Fragment2 extends Fragment {
+
+    CarouselView carouselView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment2,container,false);
+
+        carouselView = view.findViewById(R.id.carouselView);
+        carouselView.setPageCount(2);
+
+        carouselView.setImageListener(new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+                imageView.setImageResource(R.drawable.ic_android);
+            }
+        });
+
         return view;
     }
 }
