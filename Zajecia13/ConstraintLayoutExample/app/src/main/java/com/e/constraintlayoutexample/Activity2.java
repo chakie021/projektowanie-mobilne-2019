a0e71597bcb51949858fdef471849be022619d8d
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-02-07 16:43:51 +0100

    cd..

--- Zajecia13/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/Activity2.java
+++ Zajecia13/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/Activity2.java
@@ -3,12 +3,28 @@ package com.e.constraintlayoutexample;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Activity2 extends AppCompatActivity {
+
+    ArrayList<String> mStudentNames = new ArrayList<>(Arrays.asList("Imie1","Imie2","Imie3","Imie4"));
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_2);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        for(int i=5;i<100;i++)
+            mStudentNames.add("Imie"+i);
+
+
     }
 }
