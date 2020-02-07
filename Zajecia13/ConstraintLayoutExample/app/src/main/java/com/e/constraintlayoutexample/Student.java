8e8ae1b776b20ec05170dea96f86cd87a2c2a7ca
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-02-07 17:06:16 +0100

    cd..

---
+++ Zajecia13/ConstraintLayoutExample/app/src/main/java/com/e/constraintlayoutexample/Student.java
@@ -0,0 +1,19 @@
+package com.e.constraintlayoutexample;
+
+public class Student {
+    String name;
+    String surname;
+
+    Student(String name, String surname){
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+}
