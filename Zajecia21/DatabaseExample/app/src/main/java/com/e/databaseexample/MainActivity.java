f941b0065eaf34417c88ea9a46114d89280d70f4
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-02-14 17:26:51 +0100

    cd..

--- Zajecia21/DatabaseExample/app/src/main/java/com/e/databaseexample/MainActivity.java
+++ Zajecia21/DatabaseExample/app/src/main/java/com/e/databaseexample/MainActivity.java
@@ -4,11 +4,36 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    APIInterface apiInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        apiInterface = APIClient.getClient().create(APIInterface.class);
+
+        Call<List<Temperature>> call = apiInterface.getTemperature(10,1);
+
+        call.enqueue(new Callback<List<Temperature>>() {
+            @Override
+            public void onResponse(Call<List<Temperature>> call, Response<List<Temperature>> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Temperature>> call, Throwable t) {
+
+            }
+        });
+
     }
 }
