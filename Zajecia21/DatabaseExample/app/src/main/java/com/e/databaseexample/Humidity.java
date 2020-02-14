2d9410a143116f10ed2c16bd429021c789705d3e
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-02-14 17:43:00 +0100

    cd..

---
+++ Zajecia21/DatabaseExample/app/src/main/java/com/e/databaseexample/Humidity.java
@@ -0,0 +1,8 @@
+package com.e.databaseexample;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Humidity {
+    @SerializedName("wilg")
+    public Double humidity;
+}
--- Zajecia21/DatabaseExample/app/src/main/java/com/e/databaseexample/MainActivity.java
+++ Zajecia21/DatabaseExample/app/src/main/java/com/e/databaseexample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.e.databaseexample;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -26,11 +27,32 @@ public class MainActivity extends AppCompatActivity {
         call.enqueue(new Callback<List<Temperature>>() {
             @Override
             public void onResponse(Call<List<Temperature>> call, Response<List<Temperature>> response) {
+                if(response.isSuccessful()){
+                    List<Temperature> temperatures = response.body();
+
+                    for(Temperature temperature : temperatures){
+                        Log.v("Temperature: ",temperature.temperature.toString());
+                    }
+                }
 
             }
 
             @Override
             public void onFailure(Call<List<Temperature>> call, Throwable t) {
+
+            }
+        });
+
+        Call<List<Humidity>> call1 = apiInterface.getHumidity(10,1);
+
+        call1.enqueue(new Callback<List<Humidity>>() {
+            @Override
+            public void onResponse(Call<List<Humidity>> call, Response<List<Humidity>> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Humidity>> call, Throwable t) {
 
             }
         });
